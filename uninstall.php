e2d174896863100e2415c5eb0c55d0e655e91e31
Damjan Savic <starconfig09@gmail.com>
2023-03-08 15:56:28 +1100

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -4,11 +4,10 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 }
 
 // Delete custom post meta data
-$meta_keys = array('damjanwp_url', 'damjanwp_url_text');
+$meta_keys = array('damjanwp_url', 'damjanwp_url_text', 'damjanwp_cta_heading', 'damjanwp_cta_text');
 foreach ($meta_keys as $key) {
     delete_post_meta_by_key($key);
 }
 
 // Delete custom options
 delete_option('damjanwp_custom_option');
-
